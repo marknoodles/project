4cf3dc6419efc46e249cda21b76315d91e39615d
marknoodles <dambll@mail.ru>
2020-12-28 00:39:13 +0300

    13

--- src/js/test.js
+++ src/js/test.js
@@ -1,3 +1,7 @@
 function myFunction(){
     document.getElementById("dropDown-menu").classList.toggle("show");
 }
+function countClick(){
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
